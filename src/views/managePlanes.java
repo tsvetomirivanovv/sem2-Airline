df02d6a2a7e8a9cf02e6d09c4c38669f76ce739a
Dan Lepadatu <lepadatudan@gmail.com>
2016-05-22 13:55:08 +0200

    update(views/managePlanes): Fetch data from DB with getPlanes()

--- src/views/managePlanes.java
+++ src/views/managePlanes.java
@@ -2,63 +2,38 @@ package views;
 
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
-import javafx.util.converter.DoubleStringConverter;
 import javafx.util.converter.IntegerStringConverter;
 import models.Plane;
 import services.DataController;
 
 public class managePlanes extends Application{
-   // DataController dataController = new DataController();
+    DataController data = new DataController();
 
     public  Button addPlane, deletePlane,close;
     public TableView<Plane> mainPlanesTable = new TableView<>();
-    public  ObservableList<Plane> planes = FXCollections.observableArrayList(
-//            new Plane(359,"Boing", 20, 1, 5, 5, 10)
-    );
+
     @Override
     public void start(Stage primaryStage)  {
-
-
-
-
-        //LOADING FORM DATABASE
-
-
-        //tableView declaration and moving the ArrayList called <drinks> into the table
-
-        mainPlanesTable.itemsProperty().setValue(planes);
-        mainPlanesTable.setEditable(true);
-        mainPlanesTable.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY); // DESTRIBURE THE SIZE OF THE COLLUMS
-
 
         // SETTING THE COLLUMS AND ADDING THEM TO THE TABLEVIEW
         TableColumn<Plane, String> reg_num = new TableColumn<>("Registration Number");
         TableColumn<Plane, String> model = new TableColumn<>("Model");
         TableColumn<Plane, Integer> seats = new TableColumn<>("Total Seats");
-        TableColumn<Plane, Integer> inUse = new TableColumn<>("In Use");
         TableColumn<Plane, Integer> businessSeats = new TableColumn<>("Business Seats");
         TableColumn<Plane, Integer> coachSeats = new TableColumn<>("Coach Seats");
         TableColumn<Plane, Integer> economicSeats = new TableColumn<>("Economim Seats");
-
-        mainPlanesTable.getColumns().addAll(reg_num,model,seats,inUse,businessSeats,coachSeats,economicSeats);
-
 
         // ATTACHING ACTION LISTENERS (Displaying the objects into the tableview)
 
         reg_num.setCellValueFactory(e -> e.getValue().reg_noProperty());
         model.setCellValueFactory(e -> e.getValue().modelProperty());
         seats.setCellValueFactory(e -> e.getValue().seatsProperty().asObject());
-//        inUse.setCellValueFactory(e -> e.getValue().in_useProperty().asObject());
         businessSeats.setCellValueFactory(e -> e.getValue().businessSeatsProperty().asObject());
         coachSeats.setCellValueFactory(e -> e.getValue().coachSeatsProperty().asObject());
         economicSeats.setCellValueFactory(e -> e.getValue().economySeatsProperty().asObject());
@@ -69,38 +44,33 @@ public class managePlanes extends Application{
         reg_num.setCellFactory(TextFieldTableCell.forTableColumn());
         model.setCellFactory(TextFieldTableCell.forTableColumn());
         seats.setCellFactory(TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
-        inUse.setCellFactory(TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
         businessSeats.setCellFactory(TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
         coachSeats.setCellFactory(TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
         economicSeats.setCellFactory(TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
 
 
         reg_num.setOnEditCommit((TableColumn.CellEditEvent<Plane, String> event) -> {
-            ((Plane) event.getTableView().getItems().get(event.getTablePosition().getRow())).setReg_no(event.getNewValue());
+            (event.getTableView().getItems().get(event.getTablePosition().getRow())).setReg_no(event.getNewValue());
         });
 
         model.setOnEditCommit((TableColumn.CellEditEvent<Plane, String> event) -> {
-            ((Plane) event.getTableView().getItems().get(event.getTablePosition().getRow())).setModel(event.getNewValue());
+            (event.getTableView().getItems().get(event.getTablePosition().getRow())).setModel(event.getNewValue());
         });
 
         seats.setOnEditCommit((TableColumn.CellEditEvent<Plane, Integer> event) -> {
-            ((Plane) event.getTableView().getItems().get(event.getTablePosition().getRow())).setSeats(event.getNewValue());
+            (event.getTableView().getItems().get(event.getTablePosition().getRow())).setSeats(event.getNewValue());
         });
 
-//        inUse.setOnEditCommit((TableColumn.CellEditEvent<Plane, Integer> event) -> {
-//            ((Plane) event.getTableView().getItems().get(event.getTablePosition().getRow())).setIn_use(event.getNewValue());
-//        });
-
         businessSeats.setOnEditCommit((TableColumn.CellEditEvent<Plane, Integer> event) -> {
-            ((Plane) event.getTableView().getItems().get(event.getTablePosition().getRow())).setBusinessSeats(event.getNewValue());
+            (event.getTableView().getItems().get(event.getTablePosition().getRow())).setBusinessSeats(event.getNewValue());
         });
 
         coachSeats.setOnEditCommit((TableColumn.CellEditEvent<Plane, Integer> event) -> {
-            ((Plane) event.getTableView().getItems().get(event.getTablePosition().getRow())).setCoachSeats(event.getNewValue());
+            (event.getTableView().getItems().get(event.getTablePosition().getRow())).setCoachSeats(event.getNewValue());
         });
 
         economicSeats.setOnEditCommit((TableColumn.CellEditEvent<Plane, Integer> event) -> {
-            ((Plane) event.getTableView().getItems().get(event.getTablePosition().getRow())).setEconomySeats(event.getNewValue());
+            (event.getTableView().getItems().get(event.getTablePosition().getRow())).setEconomySeats(event.getNewValue());
         });
 
         Button addButton = new Button("Create ");
@@ -115,10 +85,13 @@ public class managePlanes extends Application{
 
         });
 
-
-
         VBox vBox1 = new VBox();
         vBox1.getChildren().addAll(addButton,deleteButton);
+
+        mainPlanesTable.setItems(data.getPlanes());
+        mainPlanesTable.getColumns().addAll(reg_num,model,seats,businessSeats,coachSeats,economicSeats);
+        mainPlanesTable.setEditable(true);
+        mainPlanesTable.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY); // DESTRIBURE THE SIZE OF THE COLLUMS
 
         BorderPane layout = new BorderPane();
         menu menu1 = new menu();
@@ -129,7 +102,7 @@ public class managePlanes extends Application{
 
         Scene scene = new Scene(layout, 1000, 600);
         primaryStage.setScene(scene);
-        primaryStage.setTitle("Log in as Admin");
+        primaryStage.setTitle("Manage planes");
         primaryStage.show();
 
     }
