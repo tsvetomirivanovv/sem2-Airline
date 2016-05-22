803319a75b366363ea1b25f12bd668c5f349680b
Caseru Tudor <Caseru Tudor>
2016-05-22 15:19:27 +0200

    update (views/menu) added the OSCA menu where we use OscaServices

--- src/views/menu.java
+++ src/views/menu.java
@@ -5,6 +5,7 @@ import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.stage.Stage;
+import services.OscaServices;
 
 
 public class menu {
@@ -13,12 +14,14 @@ public class menu {
         Menu filemenu = new Menu("File");
         Menu reservationsMenu = new Menu("Manage reservations");
         Menu planesMenus = new Menu("Manage Planes");
+        Menu OSCA = new Menu("OSCA");
 
         //MENU ITEMS
         MenuItem login = new MenuItem("Log in as Admin");
         MenuItem loginCustomer = new MenuItem("Log in as Customer");
         MenuItem addPlane = new MenuItem("Add Plane");
         MenuItem manageReservations = new MenuItem("Manage Reservations");
+        MenuItem OSCA1 = new MenuItem("OSCA");
 
         loginCustomer.setOnAction(event -> {
             loginAsCustomer login2 = new loginAsCustomer();
@@ -42,14 +45,19 @@ public class menu {
             manageReservations1.start(primaryStage);
         });
 
+        OSCA1.setOnAction(event -> {
+            OscaServices caos = new OscaServices();
+            caos.FlightToCSV();
+        });
 
         filemenu.getItems().addAll(login,loginCustomer);
         planesMenus.getItems().addAll(addPlane);
         reservationsMenu.getItems().addAll(manageReservations);
+        OSCA.getItems().addAll(OSCA1);
 
         //Main menu bar
         MenuBar menuBar = new MenuBar();
-        menuBar.getMenus().addAll(filemenu,reservationsMenu,planesMenus);
+        menuBar.getMenus().addAll(filemenu,reservationsMenu,planesMenus,OSCA);
         return menuBar;
 
     }
