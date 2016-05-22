10620fd6147254bfff4367974038e4c94529b885
Caseru Tudor <Caseru Tudor>
2016-05-22 15:36:38 +0200

    update (services/OscaServices) added method PlaneToCSV

--- src/services/OscaServices.java
+++ src/services/OscaServices.java
@@ -4,6 +4,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import models.Airport;
 import models.Flight;
+import models.Plane;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -17,6 +18,7 @@ import java.util.ArrayList;
 public class OscaServices {
 
     DataController x = new DataController();
+
     public void FlightToCSV () {
         PrintStream out = null;
         try {
@@ -34,6 +36,27 @@ public class OscaServices {
             out.print(timeStamp+", ");
             String timeStamp1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(list.get(i).getArrival_time());
             out.print(timeStamp1);
+            out.println();
+        }
+
+    }
+    public void PlaneToCSV () {
+        PrintStream out = null;
+        try {
+            out = new PrintStream(new File("Planes.csv"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        ObservableList<Plane> list = FXCollections.observableList(x.getPlanes());
+        for (int i=0; i<list.size(); i++) {
+            out.print(list.get(i).getPlane_id()+", ");
+            out.print(list.get(i).getReg_no()+", ");
+            out.print(list.get(i).getModel()+", ");
+            out.print(list.get(i).getBusinessSeats()+", ");
+            out.print(list.get(i).getCoachSeats()+", ");
+            out.print(list.get(i).getEconomySeats()+", ");
+            out.print(list.get(i).getBusinessPrice()+", ");
+            out.print(list.get(i).getCoachPrice());
             out.println();
         }
 
