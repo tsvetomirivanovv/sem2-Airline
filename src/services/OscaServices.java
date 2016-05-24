d03866825d1cdbc11c3e645416b93abc9baea4ba
Caseru Tudor <Caseru Tudor>
2016-05-24 21:22:39 +0200

    update (services/OscaServices) modified the printing according to Lasse's advice

--- src/services/OscaServices.java
+++ src/services/OscaServices.java
@@ -26,14 +26,16 @@ public class OscaServices {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+        out.println("//// flight_id, plane_id, departure_loc, arrival_loc, departure_time, arrival_time");
+
         ObservableList<Flight> list = FXCollections.observableList(x.getFlights());
         for (int i=0; i<list.size(); i++) {
-            out.print(list.get(i).getFlight_id()+", ");
-            out.print(list.get(i).getPlane().getPlane_id()+", ");
-            out.print(list.get(i).getDeparture_loc().getAirport_code()+", ");
-            out.print(list.get(i).getArrival_loc().getAirport_code()+", ");
+            out.print(list.get(i).getFlight_id()+",");
+            out.print(list.get(i).getPlane().getPlane_id()+",");
+            out.print(list.get(i).getDeparture_loc().getAirport_code()+",");
+            out.print(list.get(i).getArrival_loc().getAirport_code()+",");
             String timeStamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(list.get(i).getDeparture_time());
-            out.print(timeStamp+", ");
+            out.print(timeStamp+",");
             String timeStamp1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(list.get(i).getArrival_time());
             out.print(timeStamp1);
             out.println();
@@ -47,15 +49,17 @@ public class OscaServices {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+        out.println("//// plane_id, registration_no, model, businessSeats, coachSeats, economySeats, businessPrice, coachPrice");
+
         ObservableList<Plane> list = FXCollections.observableList(x.getPlanes());
         for (int i=0; i<list.size(); i++) {
-            out.print(list.get(i).getPlane_id()+", ");
-            out.print(list.get(i).getReg_no()+", ");
-            out.print(list.get(i).getModel()+", ");
-            out.print(list.get(i).getBusinessSeats()+", ");
-            out.print(list.get(i).getCoachSeats()+", ");
-            out.print(list.get(i).getEconomySeats()+", ");
-            out.print(list.get(i).getBusinessPrice()+", ");
+            out.print(list.get(i).getPlane_id()+",");
+            out.print(list.get(i).getReg_no()+",");
+            out.print(list.get(i).getModel()+",");
+            out.print(list.get(i).getBusinessSeats()+",");
+            out.print(list.get(i).getCoachSeats()+",");
+            out.print(list.get(i).getEconomySeats()+",");
+            out.print(list.get(i).getBusinessPrice()+",");
             out.print(list.get(i).getCoachPrice());
             out.println();
         }
